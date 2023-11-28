39b4f8033a5b1d62d4dcfd5e3b82dcb418ec0220
luckylxyang <liuxy0528@gmail.com>
2023-11-29 00:33:44 +0800

    注册页

--- entry/src/main/ets/utils/HttpUtil.ts
+++ entry/src/main/ets/utils/HttpUtil.ts
@@ -1,7 +1,7 @@
 // 引入包名
 import http from '@ohos.net.http';
 
-export default function httpGet(url, success, error) {
+export function httpGet(url, success, error) {
   // 每一个httpRequest对应一个HTTP请求任务，不可复用
   let httpRequest = http.createHttp();
   // 用于订阅HTTP响应头，此接口会比request请求先返回。可以根据业务需要订阅此消息
@@ -43,7 +43,65 @@ export default function httpGet(url, success, error) {
       } else{
         error(result.errorMsg)
       }
-      
+      httpRequest.off('headersReceive');
+      // 当该请求使用完毕时，调用destroy方法主动销毁
+      httpRequest.destroy();
+    } else {
+      console.info('error:' + JSON.stringify(err));
+      // 取消订阅HTTP响应头事件
+      httpRequest.off('headersReceive');
+      // 当该请求使用完毕时，调用destroy方法主动销毁
+      httpRequest.destroy();
+    }
+  }
+  );
+}
+
+export function httpPost(url,params, success, error) {
+  // 每一个httpRequest对应一个HTTP请求任务，不可复用
+  let httpRequest = http.createHttp();
+  // 用于订阅HTTP响应头，此接口会比request请求先返回。可以根据业务需要订阅此消息
+  // 从API 8开始，使用on('headersReceive', Callback)替代on('headerReceive', AsyncCallback)。 8+
+  httpRequest.on('headersReceive', (header) => {
+    console.info('header: ' + JSON.stringify(header));
+  });
+  httpRequest.request(
+    // 填写HTTP请求的URL地址，可以带参数也可以不带参数。URL地址需要开发者自定义。请求的参数可以在extraData中指定
+    url,
+    {
+      method: http.RequestMethod.GET, // 可选，默认为http.RequestMethod.GET
+      // 开发者根据自身业务需要添加header字段
+      header: {
+        'Content-Type': 'application/json'
+      },
+      // 当使用POST请求时此字段用于传递内容
+      extraData: {
+        "data": params,
+      },
+      expectDataType: http.HttpDataType.OBJECT, // 可选，指定返回数据的类型
+      usingCache: true, // 可选，默认为true
+      priority: 1, // 可选，默认为1
+      connectTimeout: 60000, // 可选，默认为60000ms
+      readTimeout: 60000, // 可选，默认为60000ms
+      usingProtocol: http.HttpProtocol.HTTP1_1, // 可选，协议类型默认值由系统自动指定
+    }, (err, data) => {
+    if (!err) {
+      // data.result为HTTP响应内容，可根据业务需要进行解析
+      console.info('Result:' + JSON.stringify(data.result));
+      console.info('code:' + JSON.stringify(data.responseCode));
+      // data.header为HTTP响应头，可根据业务需要进行解析
+      console.info('header:' + JSON.stringify(data.header));
+      console.info('cookies:' + JSON.stringify(data.cookies)); // 8+
+
+      let result = JSON.parse(JSON.stringify(data.result))
+      if (data.responseCode == 200 && result.errorCode === 0) {
+        success(result.data)
+      } else{
+        error(result.errorMsg)
+      }
+      httpRequest.off('headersReceive');
+      // 当该请求使用完毕时，调用destroy方法主动销毁
+      httpRequest.destroy();
     } else {
       console.info('error:' + JSON.stringify(err));
       // 取消订阅HTTP响应头事件
